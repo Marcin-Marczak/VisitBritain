00ef18cbf78d662a6e436e887522ac4dc33cd5c6
Marcin Marczak <marczak.krakow@gmail.com>
2021-10-27 11:26:31 +0200

    Add headless mode for chrome, firefox, edge

--- Drivers/browser_random_choice.py
+++ Drivers/browser_random_choice.py
@@ -1,5 +1,8 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from msedge.selenium_tools import Edge, EdgeOptions
 from Config.config_reader import configuration_data as cd
 
 
@@ -7,8 +10,15 @@ class BrowserRandomChoice:
     @staticmethod
     def browser_random_choice(browser):
         if browser == ['chrome']:
-            return webdriver.Chrome(executable_path=cd()["chromedriver_executable_path"])
+            chrome_options = ChromeOptions()
+            chrome_options.add_argument("--headless")
+            return webdriver.Chrome(executable_path=cd()["chromedriver_executable_path"], options=chrome_options)
         elif browser == ['firefox']:
-            return webdriver.Firefox(executable_path=cd()["firefox_executable_path"], service_log_path=os.devnull)
+            firefox_options = FirefoxOptions()
+            firefox_options.headless = True
+            return webdriver.Firefox(executable_path=cd()["firefox_executable_path"], options=firefox_options, service_log_path=os.devnull)
         elif browser == ['edge']:
-            return webdriver.Edge(executable_path=cd()["edge_executable_path"])
+            edge_options = EdgeOptions()
+            edge_options.use_chromium = True
+            edge_options.add_argument("headless")
+            return Edge(executable_path=cd()["edge_executable_path"], options=edge_options)
