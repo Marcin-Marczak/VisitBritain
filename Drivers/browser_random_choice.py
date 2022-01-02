d45d271d8acf1caaac8d4db5a77e8c751374cecc
Marcin Marczak <marczak.krakow@gmail.com>
2022-01-02 10:22:38 +0100

    Use drivers' managers instead of drivers from path in project

--- Drivers/browser_random_choice.py
+++ Drivers/browser_random_choice.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from msedge.selenium_tools import Edge, EdgeOptions
-from Config.config_reader import configuration_data as cd
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 class BrowserRandomChoice:
@@ -16,13 +18,13 @@ class BrowserRandomChoice:
         if browser == ['chrome']:
             chrome_options = ChromeOptions()
             chrome_options.add_argument("--headless")
-            return webdriver.Chrome(executable_path=cd()["chromedriver_executable_path"], options=chrome_options)
+            return webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
         elif browser == ['firefox']:
             firefox_options = FirefoxOptions()
             firefox_options.headless = True
-            return webdriver.Firefox(executable_path=cd()["firefox_executable_path"], options=firefox_options, service_log_path=os.devnull)
+            return webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=firefox_options, service_log_path=os.devnull)
         elif browser == ['edge']:
             edge_options = EdgeOptions()
             edge_options.use_chromium = True
             edge_options.add_argument("headless")
-            return Edge(executable_path=cd()["edge_executable_path"], options=edge_options)
+            return Edge(executable_path=EdgeChromiumDriverManager().install(), options=edge_options)
