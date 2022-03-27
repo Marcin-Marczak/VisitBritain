1e6209ccc911470c7f512a679b8155c18d3ea196
Marcin Marczak <marczak.krakow@gmail.com>
2022-03-27 19:15:12 +0200

    Refactor sign_in_process tests with private method usage

--- Tests/sign_in_process_test.py
+++ Tests/sign_in_process_test.py
@@ -6,28 +6,29 @@ from Config.config_reader import get_config_data as data
 
 
 @pytest.mark.usefixtures("setup")
-class SignInProcessSetup:
-    def sign_in_process_setup(self):
+class TestSignInProcess:
+    def __sign_in_process_setup(self):
         main_page = MainPage(self.driver)
+
         main_page.open_main_page_and_accept_cookies()
         main_page.go_to_sign_in_page()
 
+    def test_open_sign_in_page(self):
+        TestSignInProcess.__sign_in_process_setup(self)
 
-class TestSignInProcess(SignInProcessSetup):
-    def test_sign_in_process_open_login_page(self):
-        SignInProcessSetup.sign_in_process_setup(self)
         assert "login" in self.driver.current_url
 
     @pytest.mark.smoke
-    def test_sign_in_process_valid_username_valid_password(self):
-        SignInProcessSetup.sign_in_process_setup(self)
+    def test_sign_in_with_valid_data(self):
         sign_in = SignInPage(self.driver)
+        account = AccountPage(self.driver)
         email = data("valid_test_data.json")["email"]
         password = data("env.json")["password"]
+
+        TestSignInProcess.__sign_in_process_setup(self)
+
         sign_in.sign_in_page_fill_the_form(email, password)
         assert "Default Billing Address" in self.driver.page_source
-        account = AccountPage(self.driver)
-        account_url = self.driver.current_url
+
         account.account_page_sign_out()
-        assert self.driver.current_url != account_url
         assert "Login" in self.driver.page_source
